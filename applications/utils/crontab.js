996e978bdb5d5b6960ff2471949b536187acb1a0
franckf <efydd.artio@tuta.io>
2020-04-29 06:49:58 +0200

    sauvegarde quotidienne des bases de données

--- applications/utils/crontab.js
+++ applications/utils/crontab.js
@@ -1,7 +1,26 @@
 'use strict'
 const { CronJob } = require('cron')
+const { exec } = require('child_process')
 const { nettoyageLiensMailsetMdpOubliesBDD } = require('./../authentification/model')
 const { nettoyageStatistiquesBDD } = require('./../statistiques/model')
+
+exports.sauvegardeBasedeDonnees = () => {
+  const job = new CronJob('0 45 23 * * *', () => {
+    const dumpBDD = async (basededonnees) => {
+      try {
+        await exec(
+          `sqlite3 basededonnees/${basededonnees}.sqlite .dump > basededonnees/dumps/${basededonnees}_$(date +%Y-%m-%d).sql`
+        )
+      } catch (error) {
+        console.log(`error: ${error.message}`)
+      }
+    }
+    dumpBDD('aide')
+    dumpBDD('core')
+    dumpBDD('statistiques')
+  })
+  job.start()
+}
 
 exports.nettoyageLiensMailsetMdpOublies = () => {
   const job = new CronJob('0 0 0 * * *', () => {
