498bb87aed1856700e6cb248122b0fcb61dbf381
franckf <efydd.artio@tuta.io>
2020-05-01 16:26:27 +0200

    gestion du champs vide ou nom rempli coté client

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -121,12 +121,29 @@ const ajouterChampsNouvelEleve = () => {
 
 /* Edition eleve existant */
 
+// affichage de la modal pour editer un eleve
 const ligneEleves = document.querySelectorAll('.box-eleve')
-
 ligneEleves.forEach(ligneEleve => {
   ligneEleve.addEventListener('click', () => {
     const idEleve = ligneEleve.getAttribute('id')
     const modal = document.getElementById(`modal-eleve ${idEleve}`)
     modal.classList.add('is-active')
+  })
+})
+
+// vérification en temps réel de la longueur du nom
+const champsEleves = document.querySelectorAll('#input-eleve')
+const alertesNom = document.querySelectorAll('#alertenom')
+const buttonsSauver = document.querySelectorAll('#buttonSauver')
+
+champsEleves.forEach(element => {
+  element.addEventListener('keyup', () => {
+    if (element.value.length < 3) {
+      alertesNom.forEach(item => item.classList.remove('is-hidden'))
+      buttonsSauver.forEach(item => item.setAttribute('disabled', true))
+    } else {
+      alertesNom.forEach(item => item.classList.add('is-hidden'))
+      buttonsSauver.forEach(item => item.removeAttribute('disabled', true))
+    }
   })
 })
