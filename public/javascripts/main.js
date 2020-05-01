76443b59b396ee26d92c92e9702ef779d156f304
franckf <efydd.artio@tuta.io>
2020-05-01 16:59:50 +0200

    fichier séparer gestion eleve pour plus de clarté

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,9 @@
 import envoiInfosAnomyniseesSurUtilisation from './statistiques.js'
 import controllerModals from './modals.js'
+import gestionEleves from './gestionEleves.js'
 
 controllerModals()
+gestionEleves()
 
 /* Fenetre d'information des cookies ET la laisser fermée si validée */
 const acceptationCGU = document.querySelector('.notification-cookie')
@@ -89,41 +91,3 @@ if (!cookiesSansValeurs.includes('uuidStatsAnonym')) {
 }
 envoiInfosAnomyniseesSurUtilisation()
 */
-
-/* Gestion des eleves */
-
-/* Ajout eleve */
-const buttonAjouterEleve = document.querySelectorAll('.demanderAjoutEleve')
-const formAjoutEleve = document.querySelector('#formAjoutEleve')
-buttonAjouterEleve.forEach(button => {
-  button.addEventListener('click', () => formAjoutEleve.classList.add('is-active'))
-})
-
-/* Edition eleve existant */
-
-// affichage de la modal pour editer un eleve
-const ligneEleves = document.querySelectorAll('.box-eleve')
-ligneEleves.forEach(ligneEleve => {
-  ligneEleve.addEventListener('click', () => {
-    const idEleve = ligneEleve.getAttribute('id')
-    const modal = document.getElementById(`modal-eleve ${idEleve}`)
-    modal.classList.add('is-active')
-  })
-})
-
-// vérification en temps réel de la longueur du nom
-const champsEleves = document.querySelectorAll('#input-eleve')
-const alertesNom = document.querySelectorAll('#alertenom')
-const buttonsSauver = document.querySelectorAll('#buttonSauver')
-
-champsEleves.forEach(element => {
-  element.addEventListener('keyup', () => {
-    if (element.value.length < 3 || element.value.length > 32) {
-      alertesNom.forEach(item => item.classList.remove('is-hidden'))
-      buttonsSauver.forEach(item => item.setAttribute('disabled', true))
-    } else {
-      alertesNom.forEach(item => item.classList.add('is-hidden'))
-      buttonsSauver.forEach(item => item.removeAttribute('disabled', true))
-    }
-  })
-})
