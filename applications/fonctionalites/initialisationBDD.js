a9b58ad0212deb67c6a18d798e5fb7394a94f8ce
franckf <efydd.artio@tuta.io>
2020-05-02 15:56:27 +0200

    base de données contenant le référentiel officiel

--- applications/fonctionalites/initialisationBDD.js
+++ applications/fonctionalites/initialisationBDD.js
@@ -20,3 +20,49 @@ exports.initialisationBDDObservations = () => {
       }
     })
 }
+
+const referentiel = require('./../../config/basededonnees/referentiel')
+
+exports.initialisationBDDReferentiel = () => {
+  referentiel.schema.hasTable('domaines')
+    .then((exists) => {
+      if (!exists) {
+        return referentiel.schema.createTable('domaines', (table) => {
+          table.increments()
+            .primary()
+            .index()
+          table.string('domaine')
+        })
+      }
+    })
+  referentiel.schema.hasTable('sousdomaines')
+    .then((exists) => {
+      if (!exists) {
+        return referentiel.schema.createTable('sousdomaines', (table) => {
+          table.increments()
+            .primary()
+            .index()
+          table.integer('idDomaine')
+            .index()
+          table.foreign('idDomaine')
+            .references('domaines.id')
+          table.string('sousdomaine')
+        })
+      }
+    })
+  referentiel.schema.hasTable('observables')
+    .then((exists) => {
+      if (!exists) {
+        return referentiel.schema.createTable('observables', (table) => {
+          table.increments()
+            .primary()
+            .index()
+          table.integer('idSousDomaine')
+            .index()
+          table.foreign('idSousDomaine')
+            .references('sousdomaines.id')
+          table.string('observable')
+        })
+      }
+    })
+}
