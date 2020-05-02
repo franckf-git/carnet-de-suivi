370a9f3252ccffa30728a30effa73f4d9c1848e7
franckf <efydd.artio@tuta.io>
2020-05-02 14:35:32 +0200

    page d'entrée à la création d'un observation et ajout des icones

--- applications/fonctionalites/observation/index.js
+++ applications/fonctionalites/observation/index.js
@@ -5,5 +5,11 @@ const { siAuthentifie } = require('./../../middlewares/cookiesSessions')
 const { observation } = require('./../controller')
 
 router.get('/', siAuthentifie, observation)
+router.get('/oral', siAuthentifie, observation)
+router.get('/ecrit', siAuthentifie, observation)
+router.get('/physique', siAuthentifie, observation)
+router.get('/artistique', siAuthentifie, observation)
+router.get('/construire', siAuthentifie, observation)
+router.get('/explore', siAuthentifie, observation)
 
 module.exports = router
