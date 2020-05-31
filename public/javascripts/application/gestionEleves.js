0f84a929097b25d7480bdafe5020420b0a196695
franckf <efydd.artio@tuta.io>
2020-05-31 15:50:47 +0200

    rangement les js client uniquement dans les pages concernées

--- public/javascripts/application/gestionEleves.js
+++ public/javascripts/application/gestionEleves.js
@@ -1,39 +1,35 @@
-export function gestionEleves() {
-  /* Gestion des eleves */
+/* Ajout eleve */
+const buttonAjouterEleve = document.querySelectorAll('.demanderAjoutEleve')
+const formAjoutEleve = document.querySelector('#formAjoutEleve')
+buttonAjouterEleve.forEach(button => {
+  button.addEventListener('click', () => formAjoutEleve.classList.add('is-active'))
+})
 
-  /* Ajout eleve */
-  const buttonAjouterEleve = document.querySelectorAll('.demanderAjoutEleve')
-  const formAjoutEleve = document.querySelector('#formAjoutEleve')
-  buttonAjouterEleve.forEach(button => {
-    button.addEventListener('click', () => formAjoutEleve.classList.add('is-active'))
+/* Edition eleve existant */
+
+// affichage de la modal pour editer un eleve
+const ligneEleves = document.querySelectorAll('.box-eleve')
+ligneEleves.forEach(ligneEleve => {
+  ligneEleve.addEventListener('click', () => {
+    const idEleve = ligneEleve.getAttribute('id')
+    const modal = document.getElementById(`modal-eleve ${idEleve}`)
+    modal.classList.add('is-active')
   })
+})
 
-  /* Edition eleve existant */
+// vérification en temps réel de la longueur du nom
+const champsEleves = document.querySelectorAll('#input-eleve')
+const alertesNom = document.querySelectorAll('#alertenom')
+const buttonsSauver = document.querySelectorAll('#buttonSauver')
 
-  // affichage de la modal pour editer un eleve
-  const ligneEleves = document.querySelectorAll('.box-eleve')
-  ligneEleves.forEach(ligneEleve => {
-    ligneEleve.addEventListener('click', () => {
-      const idEleve = ligneEleve.getAttribute('id')
-      const modal = document.getElementById(`modal-eleve ${idEleve}`)
-      modal.classList.add('is-active')
-    })
+champsEleves.forEach(element => {
+  element.addEventListener('keyup', () => {
+    if (element.value.length < 3 || element.value.length > 32) {
+      alertesNom.forEach(item => item.classList.remove('is-hidden'))
+      buttonsSauver.forEach(item => item.setAttribute('disabled', true))
+    } else {
+      alertesNom.forEach(item => item.classList.add('is-hidden'))
+      buttonsSauver.forEach(item => item.removeAttribute('disabled', true))
+    }
   })
-
-  // vérification en temps réel de la longueur du nom
-  const champsEleves = document.querySelectorAll('#input-eleve')
-  const alertesNom = document.querySelectorAll('#alertenom')
-  const buttonsSauver = document.querySelectorAll('#buttonSauver')
-
-  champsEleves.forEach(element => {
-    element.addEventListener('keyup', () => {
-      if (element.value.length < 3 || element.value.length > 32) {
-        alertesNom.forEach(item => item.classList.remove('is-hidden'))
-        buttonsSauver.forEach(item => item.setAttribute('disabled', true))
-      } else {
-        alertesNom.forEach(item => item.classList.add('is-hidden'))
-        buttonsSauver.forEach(item => item.removeAttribute('disabled', true))
-      }
-    })
-  })
-}
+})
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,23 +1,15 @@
 import { acceptationCGU } from './core/acceptationCGU.js'
 import { aideEnPage } from './core/aideEnPage.js'
 import { burgerMobile } from './core/burgerMobile.js'
-import { evaluationsObservation } from './application/evaluationsObservation.js'
 import { envoiInfosAnomyniseesSurUtilisation } from './core/statistiques.js'
 import { fermetureNotifs } from './core/fermetureNotifs.js'
-import { formulaireTitreObservable } from './application/formulaireTitreObservable.js'
-import { gestionEleves } from './application/gestionEleves.js'
 import { modals } from './core/modals.js'
-import { ouvertureAccordions } from './application/ouvertureAccordions.js'
 import { retourEnHaut } from './core/retourEnHaut.js'
 
 acceptationCGU()
 aideEnPage()
 burgerMobile()
-evaluationsObservation()
 envoiInfosAnomyniseesSurUtilisation()
 fermetureNotifs()
-formulaireTitreObservable()
-gestionEleves()
 modals()
-ouvertureAccordions()
 retourEnHaut()
