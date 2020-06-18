0e566f6359e624c4a5d2c5ba827b553f28648e7e
franckf <efydd.artio@tuta.io>
2020-06-18 06:56:52 +0200

    masquer les infos cookie lors de l'impression

--- public/javascripts/application/impressionCarnet.js
+++ public/javascripts/application/impressionCarnet.js
@@ -2,6 +2,7 @@ const impression = document.querySelector('.print')
 impression.addEventListener('click', () => {
   document.querySelector('.footer').classList.add('is-hidden')
   document.querySelector('.navbar').classList.add('is-hidden')
+  document.querySelector('.notification-cookie').classList.add('is-hidden')
   document.querySelector('.tabs').classList.add('is-hidden')
   document.querySelector('.buttons-export').classList.add('is-hidden')
   document.querySelector('.button-backtotop').classList.add('is-hidden')
