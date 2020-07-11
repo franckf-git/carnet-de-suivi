166462c1348fb88b9c89e9b0702d8e5786790e3a
franckf <efydd.artio@tuta.io>
2020-07-11 10:10:23 +0200

    refuse les enregistrement des stats si il n'y a pas au moins l'uuid

--- applications/statistiques/controller.js
+++ applications/statistiques/controller.js
@@ -4,6 +4,9 @@ const logger = require('./../utils/logger')
 
 exports.infos = async (req, res, next) => {
   try {
+    if (!req.body.uuidStatsAnonym) {
+      return res.status(403).json({ message: 'enregistrement refusé - pas de uuid' })
+    }
     const infosClient = req.body
     const ip = req.ip
     const remoteAddress = req.connection.remoteAddress
